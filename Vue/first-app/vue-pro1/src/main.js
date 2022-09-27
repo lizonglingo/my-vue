33729ce55e948e2ee516963a2161170c9e91eacf
DKCBBB <40293922+lizonglingo@users.noreply.github.com>
2022-09-28 01:47:54 +0800

    feat: update

---
+++ Vue/first-app/vue-pro1/src/main.js
@@ -0,0 +1,13 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import ElementPlus from 'element-plus'
+
+import './assets/main.css'
+import 'element-plus/dist/index.css'
+
+
+const app = createApp(App)
+
+app.use(ElementPlus)
+app.mount('#app')
+

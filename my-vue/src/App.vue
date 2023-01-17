c3fed1437ef6b63ee3ab5472e7b894575a7b6ae9
DKCBBB <40293922+lizonglingo@users.noreply.github.com>
2023-01-17 23:09:34 +0800

    feat: default layout

--- my-vue/src/App.vue
+++ my-vue/src/App.vue
@@ -1,10 +1,40 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view />
+  <div class="common-layout">
+    <el-container>
+      <el-header>
+        <el-row :gutter="20">
+          <el-col :span="6">
+            <div class="grid-content ep-bg-purple">
+              这是Logo
+            </div>
+          </el-col>
+          <el-col :span="6" :offset="12">
+            <div class="grid-content ep-bg-purple-light">
+              这是头部导航选项等
+            </div>
+          </el-col>
+        </el-row>
+      </el-header>
+      <el-container>
+        <el-aside width="200px">
+          <SidebarMenu/>
+        </el-aside>
+        <el-container>
+          <el-main>
+<!--            <router-link to="/">Home</router-link> |-->
+<!--            <router-link to="/about">About</router-link>-->
+            <router-view/>
+          </el-main>
+          <el-footer>Footer</el-footer>
+        </el-container>
+      </el-container>
+    </el-container>
+  </div>
 </template>
+
+<script setup lang="ts" >
+import SidebarMenu from '@/components/SidebarMenu.vue'
+</script>
 
 <style>
 #app {
--- my-vue/vue.config.js
+++ my-vue/vue.config.js
@@ -1,4 +1,6 @@
-const { defineConfig } = require("@vue/cli-service");
+import vueSetupExtend from 'vite-plugin-vue-setup-extend'
+const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-});
+  pluginOptions: [vueSetupExtend()]
+})
